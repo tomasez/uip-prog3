f1abd24a366bc2e51fc4d092b094cf0e6b074fe9
Tomas Gutierrez <tomas.eze.g@gmail.com>
2015-07-27 14:09:40 -0300

    DML en MySQL

---
+++ Laboratorios/sem11/Investigacion  2/DML.py
@@ -0,0 +1,16 @@
+#SELECT
+SELECT column_name,column_name FROM table_name;
+
+SELECT * FROM table_name;
+#EL asterisco significa que queremos todas las columnas de la tabla
+
+#WHERE
+SELECT column_name,column_name 
+FROM table_name 
+WHERE column_name operator value;
+
+#ORDER BY
+SELECT column_name,column_name
+FROM table_name
+ORDER BY column_name,column_name ASC|DESC;
+
